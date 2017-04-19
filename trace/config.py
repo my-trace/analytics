08813660766fee6e50ee4307f6ee0d213c03b008
Alexander Zai <azai91@gmail.com>
2017-04-19 09:53:30 -0700

    add config

--- trace/config.py
+++ trace/config.py
@@ -1,30 +1,30 @@
-import os
+import os, ConfigParser
 
-def get(key):
-    try:
-        from flask import current_app
-        return current_app.config.get(key)
-    except RuntimeError:
-        return getattr(Test, key)
+config = ConfigParser.ConfigParser()
+config.read('trace/config/base.cfg')
 
-class Config(object):
+if os.environ['APP_SETTINGS'] == 'trace.config.Test':
+    print 'hello'
+    config.read('trace/config/test.cfg')    
+
+
+class DefaultConfig(object):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.environ['DEV_DB_URL']
+    SQLALCHEMY_DATABASE_URI = config.get('db', 'DB_URL')
     APP_ID = os.environ['APP_ID']
     APP_SECRET = os.environ['APP_SECRET']
     APP_TOKEN = os.environ['APP_TOKEN']
 
-class Test(Config):
+class Test(DefaultConfig):
     ENV = 'test'
     ANDY_TOKEN = os.environ.get('ANDY_TOKEN')
-    GP_API_KEY = 'TEST_GP_API_KEY'
 
-class Development(Config):
+class Development(DefaultConfig):
     ENV = 'development'
     DEBUG = True
 
-class Production(Config):
+class Production(DefaultConfig):
     ENV = 'production'
-    SQLALCHEMY_DATABASE_URI = os.environ['PROD_DB_URL']
+    SQLALCHEMY_DATABASE_URI = os.environ['DB_URL']
     GP_API_KEY = os.environ['GP_API_KEY']
 
